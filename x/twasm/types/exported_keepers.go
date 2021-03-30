f673ffb56135cdce9252c377f5b1b5b80591da07
Alex Peters <alpe@users.noreply.github.com>
2021-03-30 17:09:08 +0200

    Remove unused code/ files
    (cherry picked from commit cfbf09cc5d9b757fd7f4a07f926253044763000e)

--- x/twasm/types/exported_keepers.go
+++ x/twasm/types/exported_keepers.go
@@ -5,5 +5,4 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 // Sudoer used in abci begin block
 type Sudoer interface {
 	Sudo(ctx sdk.Context, contractAddress sdk.AccAddress, msg []byte) (*sdk.Result, error)
-	IterateContractCallbacksByType(ctx sdk.Context, callbackType PriviledgedCallbackType, cb func(prio uint64, contractAddr sdk.AccAddress) bool)
 }
