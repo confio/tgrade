b1d644c1692e9fd182de7531c32f031216a44c06
Alex Peters <alpe@users.noreply.github.com>
2021-11-04 09:48:56 +0100

    Complete query tests

--- x/poe/wasm/query_plugin_test.go
+++ x/poe/wasm/query_plugin_test.go
@@ -1,6 +1,7 @@
 package wasm
 
 import (
+	"github.com/confio/tgrade/x/twasm/types"
 	"testing"
 
 	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
@@ -15,6 +16,7 @@ import (
 )
 
 func TestStakingQuerier(t *testing.T) {
+	t.Log(types.RandomBech32Address(t))
 	specs := map[string]struct {
 		src     wasmvmtypes.StakingQuery
 		mock    ViewKeeper
@@ -166,10 +168,56 @@ func TestStakingQuerier(t *testing.T) {
   }
 }
 `,
-			// todo:
-			// - check address do not match
-			// - unknown address
-			// - invalid address
+		},
+		"query delegation - address do not match - return empty result": {
+			src: wasmvmtypes.StakingQuery{Delegation: &wasmvmtypes.DelegationQuery{Delegator: "cosmos1yq8zt83jznmp94jkj65yvfz9n52akmxt52ehm3", Validator: "cosmos17emnuddq662fpxpnd43ch0396452d48vc8ufsw"}},
+			mock: ViewKeeperMock{
+				StakeContractFn: func(ctx sdk.Context) keeper.StakeContract {
+					return poetesting.StakeContractMock{
+						QueryStakedAmountFn: func(ctx sdk.Context, opAddr sdk.AccAddress) (*sdk.Int, error) {
+							myValue := sdk.OneInt()
+							return &myValue, nil
+						},
+					}
+				},
+				GetBondDenomFn: func(ctx sdk.Context) string {
+					return "alx"
+				},
+				DistributionContractFn: func(ctx sdk.Context) keeper.DistributionContract {
+					return poetesting.DistributionContractMock{ValidatorOutstandingRewardFn: func(ctx sdk.Context, addr sdk.AccAddress) (sdk.Coin, error) {
+						return sdk.NewCoin("alx", sdk.NewInt(2)), nil
+					}}
+				},
+			},
+			expJson: `{}`,
+		},
+		"query delegation - unknown address return empty result": {
+			src: wasmvmtypes.StakingQuery{Delegation: &wasmvmtypes.DelegationQuery{Delegator: "cosmos1yq8zt83jznmp94jkj65yvfz9n52akmxt52ehm3", Validator: "cosmos1yq8zt83jznmp94jkj65yvfz9n52akmxt52ehm3"}},
+			mock: ViewKeeperMock{
+				StakeContractFn: func(ctx sdk.Context) keeper.StakeContract {
+					return poetesting.StakeContractMock{
+						QueryStakedAmountFn: func(ctx sdk.Context, opAddr sdk.AccAddress) (*sdk.Int, error) {
+							return nil, nil
+						},
+					}
+				},
+				GetBondDenomFn: func(ctx sdk.Context) string {
+					return "alx"
+				},
+			},
+			expJson: `{}`,
+		},
+		"query delegation - invalid delegator address": {
+			src:    wasmvmtypes.StakingQuery{Delegation: &wasmvmtypes.DelegationQuery{Delegator: "not a valid address", Validator: "cosmos1yq8zt83jznmp94jkj65yvfz9n52akmxt52ehm3"}},
+			expErr: true,
+		},
+		"query delegation - invalid validator address": {
+			src:    wasmvmtypes.StakingQuery{Delegation: &wasmvmtypes.DelegationQuery{Delegator: "cosmos1yq8zt83jznmp94jkj65yvfz9n52akmxt52ehm3", Validator: "not a valid address"}},
+			expErr: true,
+		},
+		"unknown query": {
+			src:    wasmvmtypes.StakingQuery{},
+			expErr: true,
 		},
 	}
 	for name, spec := range specs {
