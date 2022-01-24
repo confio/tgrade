94a91c062c381344df409ece90cfc013e95eb40d
Mauro Lacy <mauro@lacy.com.es>
2022-01-24 10:25:39 +0100

    Add ValidatorProposal types

--- x/poe/contract/tgrade_validator_voting.go
+++ x/poe/contract/tgrade_validator_voting.go
@@ -22,16 +22,48 @@ type ValidatorVotingPropose struct {
 }
 
 // ValidatorProposal proposal options.
-// Incomplete list as this is used for testing only within tgrade. Clients may use all options defined in the contract.
 type ValidatorProposal struct {
-	// PinCodes that should be pinned in cache for high performance
-	PinCodes        []uint64      `json:"pin_codes,omitempty"`
 	RegisterUpgrade *ChainUpgrade `json:"register_upgrade,omitempty"`
+	CancelUpgrade   *struct{}     `json:"cancel_upgrade,omitempty"`
+	// PinCodes that should be pinned in cache for high performance
+	PinCodes []uint64 `json:"pin_codes,omitempty"`
+	/// UnpinCodes that should be removed from cache to free space
+	UnpinCodes                    []uint64                       `json:"unpin_codes,omitempty"`
+	UpdateConsensusBlockParams    *ConsensusBlockParamsUpdate    `json:"update_consensus_block_params,omitempty"`
+	UpdateConsensusEvidenceParams *ConsensusEvidenceParamsUpdate `json:"update_consensus_evidence_params,omitempty"`
+	MigrateContract               *Migration                     `json:"migrate_contract,omitempty"`
 }
 
-// ChainUpgrade defines a subset of attributes for testing only
 type ChainUpgrade struct {
 	Name   string `json:"name"`
 	Height uint64 `json:"height"`
 	Info   string `json:"info"`
+}
+
+type ConsensusBlockParamsUpdate struct {
+	/// Maximum number of bytes (over all tx) to be included in a block
+	MaxBytes int64 `json:"max_bytes,omit_empty"`
+	/// Maximum gas (over all tx) to be executed in one block.
+	/// If set, more txs may be included in a block, but when executing, all tx after this is limit
+	/// are consumed will immediately error
+	MaxGas int64 `json:"max_gas,omitempty"`
+}
+
+type ConsensusEvidenceParamsUpdate struct {
+	/// Max age of evidence, in blocks.
+	MaxAgeNumBlocks int64 `json:"max_age_num_blocks,omitempty"`
+	/// Max age of evidence, in seconds.
+	/// It should correspond with an app's "unbonding period"
+	MaxAgeDuration int64 `json:"max_age_duration,omitempty"`
+	/// Maximum number of bytes of evidence to be included in a block
+	MaxBytes int64 `json:"max_bytes,omitempty"`
+}
+
+type Migration struct {
+	/// the contract address to be migrated
+	Contract string `json:"contract"`
+	/// a reference to the new WASM code that it should be migrated to
+	CodeId uint64 `json:"code_id"`
+	/// encoded message to be passed to perform the migration
+	MigrateMsg []byte `json:"migrate_msg"`
 }
