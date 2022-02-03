496c75b2cd638d79e2f374d5c0a6e60332428f2c
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-02-03 21:07:56 +0100

    Update download script for two repos

--- x/poe/contract/download_releases.sh
+++ x/poe/contract/download_releases.sh
@@ -8,11 +8,21 @@ if [ $# -ne 1 ]; then
 fi
 
 tag="$1"
+
 # load token from OS keychain when not set via ENV
 GITHUB_API_TOKEN=${GITHUB_API_TOKEN:-"$(security find-generic-password -a "$USER" -s "github_api_key" -w)"}
 
 rm -f version.txt
-for contract in tg4_engagement tgrade_valset tg4_mixer tg4_stake tgrade_trusted_circle tgrade_gov_reflect tgrade_oc_proposals tgrade_community_pool tgrade_validator_voting; do
+for contract in tg4_engagement tgrade_valset tg4_mixer tg4_stake tgrade_gov_reflect tgrade_community_pool tgrade_validator_voting; do
+  echo "Download $contract from poe-contracts"
+  asset_url="https://github.com/confio/poe-contracts/releases/download/${tag}/${contract}.wasm"
+  rm -f "./${contract}.wasm"
+  # download the artifact
+  echo $asset_url
+  curl -LO "$asset_url"
+done
+
+for contract in tgrade_trusted_circle tgrade_oc_proposals tgrade_ap_voting; do
   echo "Download $contract"
   list_asset_url="https://api.github.com/repos/confio/tgrade-contracts/releases/tags/${tag}"
   # get url for artifact with name==$artifact
@@ -22,5 +32,4 @@ for contract in tg4_engagement tgrade_valset tg4_mixer tg4_stake tgrade_trusted_
   curl -LJO -H 'Accept: application/octet-stream' -H "Authorization: token $GITHUB_API_TOKEN" "$asset_url"
 done
 
-rm -f version.txt
 echo "$tag" >version.txt
