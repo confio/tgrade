e504d1176674a7842b2dbd81e8c4313cf4b07af5
Mauro Lacy <mauro@confio.gmbh>
2022-03-10 08:55:11 +0100

    Fix self-delegate help message

--- x/poe/client/cli/tx.go
+++ x/poe/client/cli/tx.go
@@ -322,14 +322,14 @@ func BuildCreateValidatorMsg(clientCtx client.Context, config TxCreateValidatorC
 
 func NewDelegateCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "self-delegate [amount] [vesting-amount]",
+		Use:   "self-delegate <liquid-amount> <vesting-amount>",
 		Args:  cobra.ExactArgs(2),
 		Short: "Delegate liquid and illiquid tokens to a validator",
 		Long: strings.TrimSpace(
 			fmt.Sprintf(`Delegate an amount of liquid and/or illiquid (vesting) coins to a validator from your wallet.
 
 Examples:
-$ %s tx poe self-delegate 1000stake --from mykey
+$ %s tx poe self-delegate 1000stake 0stake --from mykey
 $ %s tx poe self-delegate 500stake 500stake --from mykey
 `,
 				version.AppName,
