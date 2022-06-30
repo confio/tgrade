eb407f30f9d6dfea4590013c5dca63edb6dda5f3
Alex Peters <alpe@users.noreply.github.com>
2022-06-30 17:12:24 +0200

    Fix TgradeSnapshotter

--- app/snapshotter.go
+++ app/snapshotter.go
@@ -1,12 +1,9 @@
 package app
 
 import (
-	"fmt"
-
 	snapshot "github.com/cosmos/cosmos-sdk/snapshots/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	protoio "github.com/gogo/protobuf/io"
-	tmos "github.com/tendermint/tendermint/libs/os"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	"github.com/CosmWasm/wasmd/x/wasm/keeper"
@@ -30,13 +27,9 @@ func (t *TgradeSnapshotter) Restore(
 	height uint64, format uint32, protoReader protoio.Reader,
 ) (snapshot.SnapshotItem, error) {
 	item, err := t.WasmSnapshotter.Restore(height, format, protoReader)
-	if err != nil {
+	if err == nil {
+		// pinned contract are initialized in WasmSnapshotter already
 		ctx := t.app.BaseApp.NewUncachedContext(true, tmproto.Header{})
-
-		// Initialize pinned codes in wasmvm as they are not persisted there
-		if err := t.app.twasmKeeper.InitializePinnedCodes(ctx); err != nil {
-			tmos.Exit(fmt.Sprintf("failed initialize pinned codes %s", err))
-		}
 		t.app.poeKeeper.InitContractAddressCache(ctx)
 	}
 	return item, err
