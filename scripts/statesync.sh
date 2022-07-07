5263dfb1eee8ea9d12110a1dd1baa482cdcb79a6
pinosu <95283998+pinosu@users.noreply.github.com>
2022-07-07 10:00:58 +0200

    Remove semicolon
    Co-authored-by: Mauro Lacy <maurolacy@users.noreply.github.com>

--- scripts/statesync.sh
+++ scripts/statesync.sh
@@ -9,7 +9,7 @@ tgrade init test --chain-id tgrade-mainnet-1
 # Get "trust_hash" and "trust_height".
 INTERVAL=2000
 SNAP_RPC="https://rpc.mainnet-1.tgrade.confio.run:443"
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height);
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height)
 BLOCK_HEIGHT=$(($LATEST_HEIGHT-$INTERVAL))
 TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
