e2526871d7eb16dfe3bf7a68cb048445f7f72004
Pino' Surace <pino.surace@live.it>
2022-07-22 14:11:54 +0200

    Add max fees limit to prevent too high fees

--- testing/fee_test.go
+++ testing/fee_test.go
@@ -40,6 +40,14 @@ func TestGlobalFee(t *testing.T) {
 	t.Log("Any transaction with enough alternative fee token amount should pass")
 	txResult = cli.CustomCommand("tx", "wasm", "store", anyContract, "--from=node0", "--gas=1500000", "--fees=150node0token")
 	RequireTxSuccess(t, txResult)
+
+	t.Log("Transactions with too high fees should fail")
+	txResult = cli.CustomCommand("tx", "wasm", "store", anyContract, "--from=node0", "--fees=150tgd")
+	RequireTxFailure(t, txResult)
+
+	t.Log("Transactions with too high fees should fail")
+	txResult = cli.CustomCommand("tx", "wasm", "store", anyContract, "--from=node0", "--gas=150", "--gas-prices=1tgd")
+	RequireTxFailure(t, txResult)
 }
 
 func TestFeeDistribution(t *testing.T) {
