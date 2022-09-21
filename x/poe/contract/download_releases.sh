e5ea27d7b0e4a5416ad22fc097286c92861124ca
pinosu <95283998+pinosu@users.noreply.github.com>
2022-09-21 13:52:41 +0200

    Upgrade Poe contracts to v0.14.0 and Tgrade contracts to v0.13.1 (#456)
    * Upgrade contracts to v0.13.0
    
    * Upgrade Poe contracts to v0.14.0 and Tgrade contracts to v0.13.1
    
    * Add claim unbondings to make test pass

--- x/poe/contract/download_releases.sh
+++ x/poe/contract/download_releases.sh
@@ -2,17 +2,18 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
-if [ $# -ne 1 ]; then
-  echo "Usage: ./download_releases.sh RELEASE_TAG"
+if [ $# -ne 2 ]; then
+  echo "Usage: ./download_releases.sh POE_RELEASE_TAG TGRADE_RELEASE_TAG"
   exit 1
 fi
 
-tag="$1"
+poe_tag="$1"
+tgrade_tag="$2"
 
 rm -f version.txt
 for contract in tg4_engagement tgrade_valset tg4_mixer tg4_stake tgrade_gov_reflect tgrade_community_pool tgrade_validator_voting; do
   echo "Download $contract from poe-contracts"
-  asset_url="https://github.com/confio/poe-contracts/releases/download/${tag}/${contract}.wasm"
+  asset_url="https://github.com/confio/poe-contracts/releases/download/${poe_tag}/${contract}.wasm"
   rm -f "./${contract}.wasm"
   # download the artifact
   echo "$asset_url"
@@ -24,7 +25,7 @@ GITHUB_API_TOKEN=${GITHUB_API_TOKEN:-"$(security find-generic-password -a "$USER
 
 for contract in tgrade_trusted_circle tgrade_oc_proposals tgrade_ap_voting; do
   echo "Download $contract"
-  list_asset_url="https://api.github.com/repos/confio/tgrade-contracts/releases/tags/${tag}"
+  list_asset_url="https://api.github.com/repos/confio/tgrade-contracts/releases/tags/${tgrade_tag}"
   # get url for artifact with name==$artifact
   asset_url=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_API_TOKEN" "${list_asset_url}" | jq -r ".assets[] | select(.name==\"${contract}.wasm\") | .url")
   rm -f "./${contract}.wasm"
@@ -32,4 +33,4 @@ for contract in tgrade_trusted_circle tgrade_oc_proposals tgrade_ap_voting; do
   curl -LJO -H 'Accept: application/octet-stream' -H "Authorization: token $GITHUB_API_TOKEN" "$asset_url"
 done
 
-echo "$tag" >version.txt
+echo -e "Poe $poe_tag\nTgrade $tgrade_tag" >version.txt
